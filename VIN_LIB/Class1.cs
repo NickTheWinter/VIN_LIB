488ee02fb01b174909f49f7eec411f30fabb7421
Зименковни <@211-12>
2023-02-22 10:02:55 +0300

    A half complete, probably

--- VIN_LIB/Class1.cs
+++ VIN_LIB/Class1.cs
@@ -4,8 +4,8 @@ namespace VIN_LIB
 {
     public class Class1
     {
-        Regex vinReg = new Regex(@"^([a-z1-9-[oiq]]{3})([a-z0-9-[oiq]]{5})([0-9x]{1})([a-y1-9-[oiqu]]{1})([a-z0-9-[oiq]]{7})$");
-        Dictionary<Regex, string> regions = new Dictionary<Regex, string>
+        static Regex vinReg = new Regex(@"^([a-z1-9-[oiq]]{3})([a-z0-9-[oiq]]{5})([0-9x]{1})([a-y1-9-[oiqu]]{1})([a-z0-9-[oiq]]{7})$",RegexOptions.IgnoreCase);
+        static Dictionary<Regex, String> regions = new Dictionary<Regex, string>
         {
             {new Regex(@"[a-h]"),"Африка"},
             {new Regex(@"[j-r]"), "Азия"},
@@ -14,7 +14,7 @@ namespace VIN_LIB
             {new Regex(@"[6-7]"), "Океания"},
             {new Regex(@"[8-9]"), "Южная Америка"}
         };
-        Dictionary<char, int> charToInt = new Dictionary<char, int>
+        static Dictionary<char, int> charToInt = new Dictionary<char, int>
         {
             {'a',1},
             {'b',2},
@@ -40,12 +40,50 @@ namespace VIN_LIB
             {'y',8},
             {'z',9}
         };
-        Boolean CheckVIN(String vin)
+        static int[] weight = { 8, 7, 6, 5, 4, 3, 2, 10, 0, 9, 8, 7, 6, 5, 4, 3, 2 };
+        public static Boolean CheckVIN(String vin)
         {
-            return true;
+            if (vinReg.Match(vin).Success)
+            {
+                vin = vin.ToLower();
+                char[] vinArray = vin.ToCharArray();
+                double contrSum = 0;
+                for (int i = 0; i < vinArray.Length; i++)
+                {
+                    if (new Regex(@"[a-z]").Match(vinArray[i].ToString()).Success)
+                    {
+                        foreach (var item in charToInt)
+                        {
+                            if (item.Key == vinArray[i])
+                                vinArray[i] = char.Parse(item.Value.ToString());
+
+                        }
+                    }
+                    contrSum += int.Parse(vinArray[i].ToString()) * weight[i];
+                }
+                double sum = contrSum;
+                contrSum = Math.Floor(contrSum / 11);
+                contrSum = contrSum * 11;
+                contrSum = sum - contrSum;
+                if (contrSum == int.Parse(vinArray[8].ToString()))
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            return false;
         }
         public static String GetVINCountry(String vin)
         {
+            vin = vin.ToLower();
+            foreach (var region in regions)
+            {
+                if (region.Key.Match(vin[0].ToString()).Success)
+                    return region.Value;
+            }
             return "";
         }
     }
--- VIN_Test/UnitTest1.cs
+++ VIN_Test/UnitTest1.cs
@@ -1,23 +1,48 @@
-
 namespace VIN_Test
 {
     [TestClass]
     public class UnitTest1
     {
         [TestMethod]
-        public void CheckVIN()
+        public void CheckJHMCM56557C404453isCorrectVIN()
         {
-            String actual = "";
-            String expected = "";
-            Assert.AreEqual(actual, expected);
+            Boolean actual;
+            Boolean expected = true;
+            actual = VIN_LIB.Class1.CheckVIN("JHMCM56557C404453");
+            Assert.AreEqual(expected, actual);
         }
         [TestMethod]
-        public void ChechJAJTisJapan()
+        public void CheckJH5CD515574404453isIncorrectVIN()
+        {
+            Boolean actual;
+            Boolean expected = false;
+            actual = VIN_LIB.Class1.CheckVIN("JH5CD515574404453");
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void ChechJHMCM56557C404453isAsia()
         {
             String actual = "";
-            String expected = "Japan";
-            actual = VIN_LIB.Class1.GetVINCountry("JA");
-            Assert.AreEqual(actual, expected);
+            String expected = "Азия";
+            actual = VIN_LIB.Class1.GetVINCountry("JHMCM56557C404453");
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void ChechJHMCM56557C404453isCorrectAndITsAsia()
+        {
+            Boolean expected = true;
+            Boolean actual;
+            Boolean first;
+            String second;
+            String expectedReg = "Азия";
+            Boolean answer = false;
+            first = VIN_LIB.Class1.CheckVIN("JHMCM56557C404453");
+            second = VIN_LIB.Class1.GetVINCountry("JHMCM56557C404453");
+            if(second.Equals(expectedReg) && first)
+            {
+                answer = true;
+            }
+            Assert.AreEqual(expected, answer);
         }
     }
 }
